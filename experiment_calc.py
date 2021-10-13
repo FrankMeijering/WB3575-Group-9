108ddc84a17bdddb4a8a7b898737919ec16b4c71
FrankMeijering <frank.meijering@ziggo.nl>
2021-10-13 21:44:05 +0200

    Added data for test 1.

--- experiment_calc.py
+++ experiment_calc.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from tools import get_folder_file, eta_comp_func
 
 # ----------------------------- DATA PROCESSING -------------------------------
-directory = get_folder_file('data', '21 09 20 14 40 23.xls')
+directory = get_folder_file('data', '21 10 12 12 23 05leeglopen_rt_10.xls')
 total_data = pd.read_csv(directory, sep='\t')
 headers = list(total_data.columns.values)
 t = np.array(total_data[headers[0]])
@@ -17,9 +17,9 @@ T1 = np.mean(np.array(total_data["temp1"])[interval1])+273.15   # [K]
 T2 = 440
 p1 = 101325    # Analog measurement   [Pa]
 p2 = 300000    # Analog measurement
-m_dot = 1e-3   # Analog measurement   [kg/s]
-torq = np.mean(np.array(total_data["torqnm"])[interval2])   # [Nm]
-rpm = np.mean(np.array(total_data["rpm"])[interval3])   # [rpm]
+m_dot_analog = 1e-3   # Analog measurement   [kg/s]
+#torq = np.mean(np.array(total_data["torqnm"])[interval2])   # [Nm]
+#rpm = np.mean(np.array(total_data["rpm"])[interval3])   # [rpm]
 
 # ------------------------------ CONSTANTS ------------------------------------
 kappa_air = 1.4     # Ratio of specific heats
@@ -29,14 +29,15 @@ cp_air = R_air/(1-1/kappa_air)   # Specific heat at constant pressure of air in 
 
 # ----------------------------- CALCULATIONS ----------------------------------
 eta_comp = eta_comp_func(T1, T2, p1, p2, kappa_air)
-power = torq*rpm*2*np.pi/60
+#power = torq*rpm*2*np.pi/60
+# m_dot_calc = power / (cp_air*(T2-T1))    # Not sure about this one
 
 # ------------------------------ PLOTTING -------------------------------------
 fig = plt.figure()
 axes = []
 i = 0
 for header in headers[1:]:
-    axes.append(fig.add_subplot(3, 4, i+1))
+    axes.append(fig.add_subplot(4, 4, i+1))
     axes[i].plot(t, np.array(total_data[header]))
     axes[i].set_title(header)
     i += 1
